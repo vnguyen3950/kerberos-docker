c284892cc6d63f873e0035bcb7bf5fc4832a4fc7
Vu Nguyen <vu.nguyen3950@gmail.com>
2020-06-12 21:36:23 +0000

    updated python authentication script

--- kerbeross.py
+++ kerbeross.py
@@ -1,3 +1,9 @@
 import requests
 from requests_kerberos import HTTPKerberosAuth, REQUIRED
-r = requests.get("http://172.24.0.2:88", auth=HTTPKerberosAuth())
+import kerberos
+def main():
+    username = input("What is your username?") + "@EXAMPLE.COM"
+    password = input("What is your password?")
+    if (kerberos.checkPassword(username, password, "EXAMPLE.COM", "EXAMPLE.COM") == True):
+        print("Authentication Successful!")
+main()
